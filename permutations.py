7745890b76ca5b95058093d063ff1f26b206f0e2
Treader7 <AymanWael7@protonmail.com>
2025-12-10 12:50:06 +0200

    Permutation tables created with comments

---
+++ permutations.py
@@ -0,0 +1,84 @@
+### DES Permutation Tables ###
+# Initial Permutation Table
+
+# Applied to the 64 bit input block at the start of the des encryption rearanging the bits according to the table
+Intial_Permutation = [
+    58, 50, 42, 34, 26, 18, 10, 2,
+    60, 52, 44, 36, 28, 20, 12, 4,
+    62, 54, 46, 38, 30, 22, 14, 6,
+    64, 56, 48, 40, 32, 24, 16, 8,
+    57, 49, 41, 33, 25, 17, 9, 1,
+    59, 51, 43, 35, 27, 19, 11, 3,
+    61, 53, 45, 37, 29, 21, 13, 5,
+    63, 55, 47, 39, 31, 23, 15, 7
+]
+
+# Final Permutation Table
+
+# Applied after 16 rounds producing the final 64 bit cipher text.
+Final_Permutation = [
+    40, 8, 48, 16, 56, 24, 64, 32,
+    39, 7, 47, 15, 55, 23, 63, 31,
+    38, 6, 46, 14, 54, 22, 62, 30,
+    37, 5, 45, 13, 53, 21, 61, 29,
+    36, 4, 44, 12, 52, 20, 60, 28,
+    35, 3, 43, 11, 51, 19, 59, 27,
+    34, 2, 42, 10, 50, 18, 58, 26,
+    33, 1, 41, 9, 49, 17, 57, 25
+]
+# Expansion Permutation Table
+
+#Expands the 32 bit right half to 48 bits for xor with round key
+Expansion_Permutation = [
+    32, 1, 2, 3, 4, 5,
+    4, 5, 6, 7, 8, 9,
+    8, 9, 10, 11, 12, 13,
+    12, 13, 14, 15, 16, 17,
+    16, 17, 18, 19, 20, 21,
+    20, 21, 22, 23, 24, 25,
+    24, 25, 26, 27, 28, 29,
+    28, 29, 30, 31, 32, 1
+]
+
+# P-Box Permutation Table
+
+# permutaion applied after 2 box substitution in each round it permutes the 32 bit output from s boxes
+P_Box = [
+    16, 7, 20, 21, 29, 12, 28, 17,
+    1, 15, 23, 26, 5, 18, 31, 10,
+    2, 8, 24, 14, 32, 27, 3, 9,
+    19, 13, 30, 6, 22, 11, 4, 25
+]
+
+
+### Key Permutation Tables ###
+# Permuted Choice 1 Table
+
+#selects 56 bits from the 64 bit key removing the 8 parity bits then splits results into 2 28 bit halves
+Permuted_Choice_1 = [
+    57, 49, 41, 33, 25, 17, 9,
+    1, 58, 50, 42, 34, 26, 18,
+    10, 2, 59, 51, 43, 35, 27,
+    19, 11, 3, 60, 52, 44, 36,
+    63, 55, 47, 39, 31, 23, 15,
+    7, 62, 54, 46, 38, 30, 22,
+    14, 6, 61, 53, 45, 37, 29,
+    21, 13, 5, 28, 20, 12, 4
+]
+
+# Permuted Choice 2 Table
+
+# selects 48 bits from the 56 bit key after shifting producing the 48 bit round key for each round
+Permuted_Choice_2 = [
+    14, 17, 11, 24, 1, 5, 3, 28,
+    15, 6, 21, 10, 23, 19, 12, 4,
+    26, 8, 16, 7, 27, 20, 13, 2,
+    41, 52, 31, 37, 47, 55, 30, 40,
+    51, 45, 33, 48, 44, 49, 39, 56,
+    34, 53, 46, 42, 50, 36, 29, 32
+]
+
+### Key Shift Schedule ###
+
+# this is used when generating the 16 round keys with each element in this table tell you how many positions to shift C and D in that round
+Shift_Schedule = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
